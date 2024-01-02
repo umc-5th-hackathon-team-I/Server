89e96b143b34991a9ff591c18aaa6ce5f72ea10f
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-01-03 04:43:12 +0900

    feat: reward 획득 - mission, reward

--- src/main/java/com/teami/domain/calendar/repository/CalendarMissionRepository.java
+++ src/main/java/com/teami/domain/calendar/repository/CalendarMissionRepository.java
@@ -4,9 +4,10 @@ import com.teami.domain.calendar.entitty.Calendar;
 import com.teami.domain.calendar.entitty.CalendarMission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface CalendarMissionRepository extends JpaRepository<CalendarMission, Long> {
-
     List<CalendarMission> findAllByCalendarOrderByDate(Calendar calendar);
+    CalendarMission findCalendarMissionByCalendarIdAndDate(Long calendarId, LocalDate date);
 }
