32839173f2c2ccb2584873fe65c591f5cd254fbc
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-01-03 00:07:57 +0900

    error: MemberRepository.java 중복 삭제

--- src/main/java/com/teami/domain/member/entitty/MemberRepository.java
+++
@@ -1,6 +0,0 @@
-package com.teami.domain.member.entitty;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
