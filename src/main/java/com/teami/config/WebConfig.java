653343291719dda8b86afdb6a04d6692a32bee6f
Junseok Kim <30834810+junseokkim@users.noreply.github.com>
2024-01-03 04:38:18 +0900

    Update WebConfig.java

--- src/main/java/com/teami/config/WebConfig.java
+++ src/main/java/com/teami/config/WebConfig.java
@@ -9,7 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://handyteam.shop")
+                .allowCredentials(true)
+                .allowedHeaders("*")
                 .allowedMethods("*")
     }
 }
